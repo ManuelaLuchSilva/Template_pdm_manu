c8ed4da1cb9b318efa2565cfcdf4b95b6fb8967d
ManuelaLuchSilva <manuelaluchtenbergsilva.ifc@gmail.com>
2025-07-14 16:23:37 -0300

    feat: inclusão do total da compra no Admin

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,14 +1,15 @@
 from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
-
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
     total = SerializerMethodField()
+
     def get_total(self, instance):
         return instance.livro.preco * instance.quantidade
+
     class Meta:
         model = ItensCompra
-        fields = ('id', 'quantidade', 'total', 'livro')
+        fields = ('livro', 'quantidade', 'total')
         depth = 1
         
 class CompraSerializer(ModelSerializer):
@@ -17,5 +18,4 @@ class CompraSerializer(ModelSerializer):
     itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
-        fields = '__all__'
-
+        fields = ('id', 'usuario', 'status', 'total', 'itens')

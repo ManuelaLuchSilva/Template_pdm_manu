9d6fd55941073c7add360ba1c1f98b96063b32fd
ManuelaLuchSilva <manuelaluchtenbergsilva.ifc@gmail.com>
2025-09-02 14:45:38 -0300

    feat: criação de uma compra a partir do usuário autenticado

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, CurrentUserDefault, HiddenField, ModelSerializer, SerializerMethodField
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):

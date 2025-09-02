3ee710db7b0555865cf4ee0d2b0a4c7800145365
ManuelaLuchSilva <manuelaluchtenbergsilva.ifc@gmail.com>
2025-09-02 15:03:12 -0300

    feat: validando e formatando dados antes de salvar

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, CurrentUserDefault, HiddenField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, CurrentUserDefault, HiddenField, ModelSerializer, SerializerMethodField, ValidationError
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
@@ -16,6 +16,15 @@ class ItensCompraCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ('livro', 'quantidade')
+
+    def validate_quantidade(self, quantidade):
+        if quantidade <= 0:
+            raise ValidationError('A quantidade deve ser maior do que zero.')
+        return quantidade
+    def validate(self, item):
+        if item['quantidade'] > item['livro'].quantidade:
+            raise ValidationError('Quantidade de itens maior do que a quantidade em estoque.')
+        return item
         
 class ItensCompraListSerializer(ModelSerializer):
     livro = CharField(source='livro.titulo', read_only=True)
